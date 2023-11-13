88d3e8dd37b85805b8f1b38049412c0fcb598429
Your Name <tajempey@gmail.com>
2023-11-13 16:48:11 -0700

    should calculate the years until a selected year on uranus

--- __tests__/calc.test.js
+++ __tests__/calc.test.js
@@ -80,6 +80,12 @@ describe('getYearsUntil', () => {
     let yearsUntil = (yearsUntilTest.getYearsUntil());
     expect(yearsUntil.saturnAge).toEqual(0.34);
   });
+
+  test('should calculate the years until a selected year on uranus', () => {
+    const yearsUntilTest = new UserInput(40, 50, 30);
+    let yearsUntil = (yearsUntilTest.getYearsUntil());
+    expect(yearsUntil.uranusAge).toEqual(0.48);
+  });
 });
 
 describe('getYearSince', () => {
