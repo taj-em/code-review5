bf6d2b858df903f415a7a7e44e0651fba40760cb
Your Name <tajempey@gmail.com>
2023-11-12 20:09:00 -0700

    create a new object using two inputted values

--- __tests__/calc.test.js
+++ __tests__/calc.test.js
@@ -1,11 +1,11 @@
 /* eslint-disable no-undef */
 import { UserInput } from './../src/calc';
 
-describe('UserInput'), () => {
+describe('UserInput', () => {
 
   test('should create an object containing the users initial age and future age', () => {
-    const inputTest = new UserInput(40, 50);
+    let inputTest = new UserInput(40, 50);
     expect(inputTest.age1).toEqual(40);
     expect(inputTest.age2).toEqual(50); 
   });
-}
+});
---
+++ src/calc.js
@@ -0,0 +1,7 @@
+/* eslint-disable no-unused-vars */
+export class UserInput {
+  constructor(age1, age2) {
+    this.age1 = age1;
+    this.age2 = age2;
+  }
+}
