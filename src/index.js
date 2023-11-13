dc16456d4857f4a2f5f9d42a5ae162f39f0fc2b9
Your Name <tajempey@gmail.com>
2023-11-13 12:17:22 -0700

    WIP

---
+++ src/index.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-unused-vars */
+import { UserInput } from "./calc";
+
+function clicked() {
+  window.alert();
+}
+
+document.getElementById("click-me").addEventListener("click", clicked)
