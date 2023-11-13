aace665cb7c23c3082d3905cfafa9b9dd23b9920
Your Name <tajempey@gmail.com>
2023-11-13 09:39:10 -0700

    should calculate the years until a selected year on mercury

--- __tests__/calc.test.js
+++ __tests__/calc.test.js
@@ -4,7 +4,7 @@ import { UserInput } from './../src/calc';
 describe('UserInput', () => {
 
   test('should create an object containing the users initial age and future age', () => {
-    let inputTest = new UserInput(40, 50);
+    let inputTest = new UserInput(40, 50, 30);
     expect(inputTest.age1).toEqual(40);
     expect(inputTest.age2).toEqual(50); 
   });
@@ -13,26 +13,35 @@ describe('UserInput', () => {
 describe('getCurrentAge', () => {
 
   test('should convert age1 into same age on mercury', () => {
-    let currentAgeTest = new UserInput(40, 50);
+    let currentAgeTest = new UserInput(40, 50, 30);
     let plantAges = (currentAgeTest.getCurrentAge());
     expect(plantAges.mercuryAge).toEqual(166);
   });
 
   test('should convert age1 into same age on venus', () => {
-    let currentAgeTest = new UserInput(40, 50);
+    let currentAgeTest = new UserInput(40, 50, 30);
     let plantAges = (currentAgeTest.getCurrentAge());
     expect(plantAges.venusAge).toEqual(65);
   });
 
   test('should convert age1 into same age on mars', () => {
-    let currentAgeTest = new UserInput(40, 50);
+    let currentAgeTest = new UserInput(40, 50, 30);
     let plantAges = (currentAgeTest.getCurrentAge());
     expect(plantAges.marsAge).toEqual(21);
   });
 
   test('should convert age1 into same age on jupiter', () => {
-    let currentAgeTest = new UserInput(40, 50);
+    let currentAgeTest = new UserInput(40, 50, 30);
     let plantAges = (currentAgeTest.getCurrentAge());
     expect(plantAges.jupiterAge).toEqual(3);
+  });
+});
+
+describe('getYearsUntil', () => {
+
+  test('should calculate the years until a selected year on mercury', () => {
+    const yearsUntilTest = new UserInput(40, 50, 30);
+    let yearsUntil = (yearsUntilTest.getCurrentAge());
+    expect(yearsUntil.mercuryAge).toEqual(42);
   });
 });
