d7d7dc3459f460cd8b169981f587cf927b8dc0e7
Your Name <tajempey@gmail.com>
2023-11-13 17:36:40 -0700

    should throw an error if age1 is less than 0.1

--- src/calc.js
+++ src/calc.js
@@ -23,20 +23,24 @@ export class UserInput {
   }
 
   getYearsUntil() {
-    const earthDays = this.age1 * 365;
-    const futureEarthDays = this.age2 * 365;
-    let yearsUntil = {};
-    yearsUntil.neptuneAge = parseFloat(((futureEarthDays / 60189.55) - (earthDays / 60189.55)).toFixed(2));
-    yearsUntil.uranusAge = parseFloat(((futureEarthDays / 30684.65) - (earthDays / 30684.65)).toFixed(2));
-    yearsUntil.saturnAge = parseFloat(((futureEarthDays / 10752.9) - (earthDays / 10752.9)).toFixed(2));
-    yearsUntil.jupiterAge = parseFloat(((futureEarthDays / 4328.9) - (earthDays / 4328.9)).toFixed(2));
-    yearsUntil.plutoAge = parseFloat(((futureEarthDays / 90797.5) - (earthDays / 90797.5)).toFixed(2));
-    yearsUntil.mercuryAge = parseFloat(((futureEarthDays / 87.97) - (earthDays / 87.97)).toFixed(2));
-    yearsUntil.venusAge = parseFloat(((futureEarthDays / 224.7) - (earthDays / 224.7)).toFixed(2));
-    yearsUntil.marsAge = parseFloat(((futureEarthDays / 686.2) - (earthDays / 686.2)).toFixed(2));
-    return yearsUntil;
+    if (this.age1 < 0.1) {
+      return "Error";
+    } else {
+      const earthDays = this.age1 * 365;
+      const futureEarthDays = this.age2 * 365;
+      let yearsUntil = {};
+      yearsUntil.neptuneAge = parseFloat(((futureEarthDays / 60189.55) - (earthDays / 60189.55)).toFixed(2));
+      yearsUntil.uranusAge = parseFloat(((futureEarthDays / 30684.65) - (earthDays / 30684.65)).toFixed(2));
+      yearsUntil.saturnAge = parseFloat(((futureEarthDays / 10752.9) - (earthDays / 10752.9)).toFixed(2));
+      yearsUntil.jupiterAge = parseFloat(((futureEarthDays / 4328.9) - (earthDays / 4328.9)).toFixed(2));
+      yearsUntil.plutoAge = parseFloat(((futureEarthDays / 90797.5) - (earthDays / 90797.5)).toFixed(2));
+      yearsUntil.mercuryAge = parseFloat(((futureEarthDays / 87.97) - (earthDays / 87.97)).toFixed(2));
+      yearsUntil.venusAge = parseFloat(((futureEarthDays / 224.7) - (earthDays / 224.7)).toFixed(2));
+      yearsUntil.marsAge = parseFloat(((futureEarthDays / 686.2) - (earthDays / 686.2)).toFixed(2));
+      return yearsUntil;
+    }
   }
-
+  
   getYearSince() {
     const earthDays = this.age1 * 365;
     const pastEarthDays = this.age3 * 365;
